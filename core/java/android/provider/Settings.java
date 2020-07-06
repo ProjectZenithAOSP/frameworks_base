96c374cd674e11f66cb0b4762509894edd42bbdd
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-07-06 01:35:10 +0000

    SystemUI: Use app icons for notifications in statusbar
    Squashed:
    
        From: spkal01 <spkal01@users.noreply.github.com>
        Date: Thu, 9 Dec 2021 18:32:21 +0000
        Subject: SystemUI: Make app icons in statusbar optional [1/2]
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 15 Jan 2022 17:09:09 +0530
        Subject: SystemUI: Refactor colored statusbar icons
    
        * Remove painful systemui checks everywhere in StatusBariconView
        * Apply NO_COLOR when colored icons are enabled.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [neobuddy89: Updated for A14 QPR1, A15]
    
    Change-Id: I90cde2d2ae2fdf228ee167e9b24d2316e97a61a7
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6636,6 +6636,12 @@ public final class Settings {
         public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
