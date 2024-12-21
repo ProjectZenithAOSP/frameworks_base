d58e31324e009ae9fd8690514f4e63f54f198454
beanstown106 <nbenis106@gmail.com>
2024-12-22 00:50:06 +0530

    Fingerprint authentication vibration [1/2]
    afterallafk edit's-
    Adapt to Android 15 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6639,6 +6639,18 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether to vibrate on succesful fingerprint authentication
+         * @hide
+         */
+        public static final String FP_SUCCESS_VIBRATE = "fp_success_vibrate";
+        
+        /**
+         * Whether to vibrate on unsuccesful fingerprint authentication
+         * @hide
+         */
+        public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
+
+        /**
          * These are all public system settings
          *
          * @hide
